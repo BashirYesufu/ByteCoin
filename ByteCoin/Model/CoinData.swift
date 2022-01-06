1da71dbb72115391b7608257b0184c7d68025a68
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-06 18:09:09 +0100

    Added network call functionality

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by mac on 1/6/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
